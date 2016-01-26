ed70c7d6e04aa8c87d0baa23f4846a9c157ffb47
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2016-01-26 16:58:24 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var STS = require('ali-oss').STS;
+var co = require('co');
+var fs = require('fs');
+var app = express();
+
+app.get('/', function (req, res) {
+  var conf = JSON.parse(fs.readFileSync('./config.json'));
+  var policy;
+  if (conf.PolicyFile) {
+    policy = fs.readFileSync(conf.PolicyFile).toString('utf-8');
+  }
+
+  var client = new STS({
+    accessKeyId: conf.AccessKeyId,
+    accessKeySecret: conf.AccessKeySecret,
+  });
+
+  co(function* () {
+    var result = yield client.assumeRole(conf.RoleArn, policy, conf.TokenExpireTime);
+    console.log(result);
+
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-METHOD', 'GET');
+    res.json({
+      AccessKeyId: result.credentials.AccessKeyId,
+      AccessKeySecret: result.credentials.AccessKeySecret,
+      Security: result.credentials.SecurityToken,
+      Expiration: result.credentials.Expiration
+    });
+  }).then(function () {
+    // pass
+  }).catch(function (err) {
+    console.log(err);
+    res.status(400).json(err.message);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('App started.');
+});
